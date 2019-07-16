4636b45f3a493e900188f0c5a15f40b431a9f690
acw1251 <acwright@mit.edu>
2019-07-16 16:17:59 -0400

    Bump version to v0.1.1 in preparation for next release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='PyVerilator',
-    version='0.1.0',
+    version='0.1.1',
     description='Python interface to Verilator models',
     long_description=long_description,
     url='https://github.com/csail-csg/pyverilator',
