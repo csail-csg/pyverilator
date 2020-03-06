fcd0d704e8e772518a74304794013791d0601888
acw1251 <acwright@mit.edu>
2020-03-06 10:26:09 -0500

    Bumped version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='PyVerilator',
-    version='0.6.0',
+    version='0.7.0',
     description='Python interface to Verilator models',
     long_description=long_description,
     url='https://github.com/csail-csg/pyverilator',
