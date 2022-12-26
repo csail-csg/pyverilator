deb478fc2bddb2e878a6431e1e30e0ef53ddf097
Marco Merlin <marcomerli+github@gmail.com>
2022-12-26 23:14:53 +0100

    Create verilator_tools.py

---
+++ pyverilator/verilator_tools.py
@@ -0,0 +1,17 @@
+def verilator_version():
+    import subprocess
+    result = subprocess.run(['verilator', '--version'], stdout=subprocess.PIPE)
+    ver = result.stdout.split()[1]
+    return ver.decode("utf-8") 
+
+def verilator_flushcall_ok():
+    # check Verilated::flushCall() exist
+    # https://github.com/chipsalliance/chisel3/issues/1565
+    from packaging import version
+    ver = verilator_version()
+    # print(ver)
+    if ( version.parse(ver) < version.parse("4.036") or
+         version.parse(ver) > version.parse("4.102")):
+        return True
+    else:
+        return False
